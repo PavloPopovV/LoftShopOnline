6ad7d923bf460932ad11d248d2174fcc8becd604
Denys <den.popov1002@gmail.com>
2024-03-12 16:21:20 +0200

    Catalog-page

--- app/js/main.js
+++ app/js/main.js
@@ -7,14 +7,14 @@ function changeHeaderTextColorWhite(className) {
   [...productPageHeader].forEach((btn) => btn.classList.add("inner-header"));
 }
 
-function headerScrool() {
+function headerScroll() {
   const scrollTop = wrapper.scrollTop;
   header.classList.toggle("sticky", scrollTop >= 50);
   header.classList.toggle("animation", scrollTop >= 200);
   headerCount.classList.toggle("color", scrollTop >= 200);
 }
 
-function checkClass() {
+function checkClassForHeaderOnLoad() {
   if (document.querySelector(".inner-page") !== null) {
     changeHeaderTextColorWhite(".header__btn");
     changeHeaderTextColorWhite(".header__logo");
@@ -28,15 +28,11 @@ function showOrHideSwiper() {
   const sliders = document.querySelectorAll(".swiper");
   const arrows = document.querySelectorAll(".carousel__arrow");
 
-  if (url === "/" && wrapper.clientWidth < 760) {
+  if ((url === "/" || url === "/index.html") && wrapper.clientWidth < 760) {
     sliders.forEach((item) => {
       item.classList.remove("swiper");
       item.firstElementChild.classList.remove("swiper-wrapper");
     });
-<<<<<<< HEAD
-=======
-
->>>>>>> dcfb5f570acfecf44a0b08c6d21adefd51c8d89e
     arrows.forEach((arrow) => (arrow.style.display = "none"));
     return;
   }
@@ -103,13 +99,22 @@ document.addEventListener("click", (e) => {
   if (target.classList.contains("mobile__close")) {
     document.querySelector(".mobile").classList.remove("active");
   }
+  if (target.classList.contains("catalog__input")) {
+    document.querySelector(".search-popup").classList.toggle("show");
+  }
+
+  if (target.classList.contains("js-add__subcategories")) {
+    target.classList.toggle("active");
+    target.nextElementSibling.classList.toggle("show");
+  }
 });
 
 document.addEventListener("DOMContentLoaded", () => {
   showOrHideSwiper();
-  checkClass();
+  checkClassForHeaderOnLoad();
+  changeFiltersOnLoad();
 });
 
 wrapper.addEventListener("scroll", function () {
-  headerScrool();
+  headerScroll();
 });
